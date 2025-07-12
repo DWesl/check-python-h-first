7eabb6a5ee22af16ef94418a850834a5771ceda4
DWesl <22566757+DWesl@users.noreply.github.com>
2025-07-11 22:02:20 -0400

    TST: Make tests more robust for GHA CI.
    Shallow clones and different directory ordering messed things up.
    They really shouldn't.

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -2,6 +2,9 @@
 
 import glob
 import os.path
+import subprocess
+
+import pytest
 
 from check_python_h_first.wrapper import (
     diff_files,
@@ -19,7 +22,11 @@ SOURCE_LIST = glob.glob(os.path.join(THIS_DIR, "*.c"))
 def test_sort_order():
     """Test that the sort function puts headers first."""
     result = sorted(SOURCE_LIST + HEADER_LIST, key=sort_order)
-    assert result == HEADER_LIST + SOURCE_LIST
+    # assert result == HEADER_LIST + SOURCE_LIST
+    result_extensions = [os.path.splitext(name)[1] for name in result]
+    c_index = result_extensions.index(".c")
+    assert all(ext == ".h" for ext in result_extensions[:c_index])
+    assert all(ext == ".c" for ext in result_extensions[c_index:])
 
 
 def test_find_c_cpp_files():
@@ -40,6 +47,11 @@ def test_process_files():
     )
 
 
+@pytest.mark.xfail(
+    os.environ.get("GITHUB_ACTIONS", "false") == "true",
+    reason="GHA does a shallow clone, without the history needed for this test.",
+    raises=subprocess.CalledProcessError,
+)
 def test_diff_files():
     """Test whether diff_files picks up the correct files."""
     new_files = [os.path.basename(name) for name in diff_files("c8bda44")]
