8538fd7eb39454a0078ca57d9f58ebbfbadd563c
DWesl <22566757+DWesl@users.noreply.github.com>
2025-08-08 01:12:33 -0400

    MNT: Update docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev29+g469d8ff.d20250712',
+    VERSION: '0.0.post1.dev32+g52f497b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
